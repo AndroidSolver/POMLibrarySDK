0c50669fdaa4d67feabda1170e2579caa4e6406c
Sridhar <sgovindaraju@denovosystem.com>
2022-08-05 15:19:11 +0530

    Initail commit for POMLIB SDK

---
+++ app/src/main/java/com/denovo/pomlibapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.denovo.pomlibapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppCompatImageView appCompatImageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        appCompatImageView = findViewById(R.id.imgLogo);
+        Glide.with(this).load("https://picsum.photos/id/237/200/300").into(appCompatImageView);
+
+    }
+}
