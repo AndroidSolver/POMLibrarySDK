43ccf70e4e677a6f855d945be4511a1c53302eb4
Sridhar <sgovindaraju@denovosystem.com>
2022-08-05 17:21:40 +0530

    textview called

--- app/src/main/java/com/denovo/pomlibapp/MainActivity.java
+++ app/src/main/java/com/denovo/pomlibapp/MainActivity.java
@@ -4,19 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppCompatImageView appCompatImageView;
-    @Override
+    private TextView txtLogo;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        appCompatImageView = findViewById(R.id.imgLogo);
-        Glide.with(MainActivity.this).load("https://picsum.photos/id/237/200/300").into(appCompatImageView);
+        txtLogo = findViewById(R.id.txtLogo);
+        txtLogo.setText("SDK page starts");
+        //Glide.with(MainActivity.this.getApplicationContext()).load("https://picsum.photos/id/237/200/300").into(appCompatImageView);
 
     }
 }
