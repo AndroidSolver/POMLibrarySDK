74353d1f40fb0fbabc25e730f6070d0c0d156bef
Sridhar <sgovindaraju@denovosystem.com>
2022-08-05 16:50:50 +0530

    NEW TASK flag produced

--- app/src/main/java/com/denovo/pomlibapp/SdkService.java
+++ app/src/main/java/com/denovo/pomlibapp/SdkService.java
@@ -13,6 +13,7 @@ public class SdkService {
 
     public void openMainActivity() {
         Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
     }
 }
