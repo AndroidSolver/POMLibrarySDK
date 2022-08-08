9bf8524ecdbd24e92cb116552bd7e18a2611eb9e
Sridhar <sgovindaraju@denovosystem.com>
2022-08-08 13:39:02 +0530

    Imageview added

--- app/src/main/java/com/denovo/pomlibapp/MainActivity.java
+++ app/src/main/java/com/denovo/pomlibapp/MainActivity.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView txtLogo;
+    //private TextView txtLogo;
+    private ImageView imageView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
         //txtLogo = findViewById(R.id.txtLogo);
         //txtLogo.setText("SDK page starts");
-        //Glide.with(MainActivity.this.getApplicationContext()).load("https://picsum.photos/id/237/200/300").into(appCompatImageView);
+        imageView = findViewById(R.id.imageView);
+        Glide.with(MainActivity.this).load("https://picsum.photos/id/237/200/300").into(imageView);
 
     }
 }
