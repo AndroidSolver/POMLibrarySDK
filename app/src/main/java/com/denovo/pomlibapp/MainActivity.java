860a9600f2cbbb4c12825c81019ef6601c05af3c
Sridhar <sgovindaraju@denovosystem.com>
2022-08-09 13:16:46 +0530

    Common language and collection gradles are added

--- app/src/main/java/com/denovo/pomlibapp/MainActivity.java
+++ app/src/main/java/com/denovo/pomlibapp/MainActivity.java
@@ -9,17 +9,19 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 public class MainActivity extends AppCompatActivity {
 
-    //private TextView txtLogo;
+    private TextView txtLogo;
     private ImageView imageView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //txtLogo = findViewById(R.id.txtLogo);
-        //txtLogo.setText("SDK page starts");
+        txtLogo = findViewById(R.id.txtLogo);
+        txtLogo.setText(StringEscapeUtils.unescapeHtml3("SDK page starts"));
         imageView = findViewById(R.id.imageView);
         Glide.with(MainActivity.this).load("https://picsum.photos/id/237/200/300").into(imageView);
 
